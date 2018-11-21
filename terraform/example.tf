0a4575068a629bd1391c04c4d88da89211097726
Sebastian Dunne <sdunne@x1cg6.sdunne.net>
2018-11-20 21:46:46 -0500

    asd

--- terraform/example.tf
+++ terraform/example.tf
@@ -8,3 +8,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0af6f794ec2d5ff13"
   instance_type = "t2.small"
 }
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
